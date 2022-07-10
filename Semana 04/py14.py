41fcdbf34fff4a4b032681c54efe757a0f004ace
alisson <AlissonCV>
2022-07-10 09:30:38 -0300

    Atividade Semana 04 print

--- Semana 04/py14.py
+++ Semana 04/py14.py
@@ -7,23 +7,99 @@ os.chdir('/home/alisson/Documentos/Sistemas Embarcados II/SEII-Alisson-Carvalho-
 
 print(os.getcwd())
 
-print(dir(os))
-
 for f in os.listdir():
     print(f)
+
+print()
+
+for f in os.listdir():
     print(os.path.splitext(f))
+
+print()
+
+for f in os.listdir():
     f_name, f_ext = os.path.splitext(f)
     print(f_name)
-    print(f_ext)
-    print(file_name.split('-'))
-    f_title, f_course, f_num = file_name.split('-')
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    print(f_name.split('-'))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
     print(f_title)
-    print(f_couse)
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    print(f_course)
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
     print(f_num)
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    print('{}-{}-{}{}'.format(f_num, f_course, f_title, f_ext))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
     f_title = f_title.strip()
-    f_couse = f_couse.strip()
+    f_course = f_course.strip()
+    f_num = f_num.strip()
+    print('{}-{}-{}{}'.format(f_num, f_course, f_title, f_ext))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    f_title = f_title.strip()
+    f_course = f_course.strip()
+    f_num = f_num.strip()[1:]
+    print('{}-{}-{}{}'.format(f_num, f_course, f_title, f_ext))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    f_title = f_title.strip()
+    f_course = f_course.strip()
     f_num = f_num.strip()[1:].zfill(2)
     print('{}-{}-{}{}'.format(f_num, f_course, f_title, f_ext))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    f_title = f_title.strip()
+    f_num = f_num.strip()[1:].zfill(2)
     print('{}-{}{}'.format(f_num, f_title, f_ext))
-    new_name = '{}-{}{}'.format(f_num, f_title, f_ext))
+
+print()
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_course, f_num = f_name.split('-')
+    f_title = f_title.strip()
+    f_num = f_num.strip()[1:].zfill(2)
+    new_name = '{}-{}{}'.format(f_num, f_title, f_ext)
     os.rename(f, new_name)
