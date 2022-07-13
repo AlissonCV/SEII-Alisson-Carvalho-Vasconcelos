58d4be89be071a4c04c5c279efeb0eed0acb4a4a
alisson <AlissonCV>
2022-07-13 11:55:18 -0300

    Atividade Semana04 numpy03

--- Semana 04/numpy03.py
+++ Semana 04/numpy03.py
@@ -2,37 +2,42 @@
 #Vídeo https://www.youtube.com/watch?v=DcfYgePyedM
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-a1 = np.array([3,5,7,3]) #cria um array de números
+a1 = np.array([2,4,6,8,10]) #cria um array de números
 
-print('a1 =', a1)
+print('\na[2]:',a1[2]) #mostra o terceiro elemento
 
-print()
-a2 = np.zeros(10) #cria um array de 10 elementos iguais a zero
+print('\na1[2:]:',a1[2:]) #mostra do terceiro elemento ao último
 
-print('a2 =', a2)
+print('\na1[:-2]:', a1[:-2]) #mostra os elementos excluindo os 2 últimos
 
-print()
-a3 = np.ones(10) #cria um array de 10 elementos iguais a um
+print('\na1[1:-2]:', a1[1:-2]) #mostra os elementos inicando no segundo e excluindo os 2 últimos
 
-print('a3 =',a3)
+print('\na1[::2]:', a1[::2]) #mostra os elementos iniciando do primeiro e espaçado de 2
 
-print()
-a4 = np.random.random(10) #array de números randômicos uniformemente variando de 0 a 1
+print('\na1>3:', a1>3) #comparação dos elementos
 
-print('a4 =', a4)
+print('\na1[a1>3]:', a1[a1>3]) #seleciona os elementos que satisfazem a condição >3
 
-print()
-a5 = np.random.randn(10) #array de números gaussianos normais distribuidos com uma variação em torno de zero
+names = np.array(['Jim', 'Luke', 'Josh', 'Pete']) #cria um array de strings
 
-print('a5 =', a5)
+print('\nnames:', names)
 
-print()
-a6 = np.linspace(0, 10, 100) #array de números variando de 0 a 10 espaçados igualmente
+'''
+class numpy.vectorize(pyfunc (python function or method), otypes=None, doc=None, excluded=None, cache=False,
+signature=None) -> Define uma função vetorizada que recebe uma sequência aninhada de objetos ou matrizes numpy
+como entradas e retorna uma única matriz numpy ou uma tupla de matrizes numpy. A função vetorizada avalia pyfunc
+sobre tuplas sucessivas das matrizes de entrada, como a função python map, exceto se usar as regras de transmissão de numpy.
+'''
 
-print('a6:\n', a6)
+first_letter_j = np.vectorize(lambda s: s[0])(names)=='J' # a função lambda pega o primeiro caracter da string "s", a função
+							  #np.vectorize faz um loop de todos os elementos do array names,  e
+							  #compara se são iguais a 'J'
 
-print()
-a7 = np.arange(0, 10, 0.02) #array de números variando de 0 a 10 espaçados de 0.02
+print('\nfirst_letter_j:', first_letter_j)
+print('\nnames[first_letter]:', names[first_letter_j]) #cria um array com as strings da variável names que começa com "J"
 
-print('a7:\n', a7)
+print('\na1%4:', a1%4)
+print('\na1%4==0:', a1%4==0) #cria um array boleano indicando se os elementos de a1 são ou não divisíveis por 4
+print('\na1[a1%4==0]:', a1[a1%4==0]) #cria um array de valores que são divisíveis por 4 e possui o resto igual a zero
